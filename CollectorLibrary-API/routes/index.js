f638b12f1a817b381cc72a44193ed617e9df9b22
nicky18-txt <nic.altamirano@unitec.edu>
2026-02-24 17:35:06 -0600

    Scaffold for Express

---
+++ CollectorLibrary-API/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(request, response, next) {
+  response.render('index', { title: 'Nicole' });
+});
+
+module.exports = router;
